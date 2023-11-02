859a5e089bbb1eadc698a5f92625ff1206e7ac72
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-11-02 09:11:41 +0530

    finished Custom hooks

---
+++ src/tutorial/08-custom-hooks/starter/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+//Making a generic useFetch custom hook out of useFetchPerson custom hook
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(url);
+        // console.log(resp);
+        if (!resp.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+
+        const response = await resp.json();
+        setData(response);
+      } catch (error) {
+        setIsError(true);
+        // console.log(error);
+      }
+      // hide loading
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  return { isLoading, isError, data };
+};
+
+export default useFetch;
