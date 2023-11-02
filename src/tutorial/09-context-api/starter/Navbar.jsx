1b7a9595ae865763683397fc56e4c9a40f1c9047
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-11-02 17:42:24 +0530

    Completed ContextAPI and Prop-drilling

---
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import NavLinks from "./NavLinks";
+import { useContext } from "react";
+
+export const NavbarContext = createContext();
+
+//Custom Hook for navabarcontext
+export const useAppContext = () => useContext(NavbarContext);
+
+const Navbar = () => {
+  const [user, setUser] = useState({ name: "Bob" });
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <NavbarContext.Provider value={{user, logout}}>
+      <nav className="navbar">
+        <h5>CONTEXT API</h5>
+        <NavLinks />
+      </nav>
+    </NavbarContext.Provider>
+  );
+};
+export default Navbar;
--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useReducer } from "react";
+import { data } from "../../../data";
+
+const defaultState = { people: data };
+
+const reducer = () => {};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
+
+  const resetList = () => {
+    // setPeople(data);
+  };
+
+  const clearList = () => {};
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {state.people.length === 0 ? (
+        <button type="button" className="btn" onClick={resetList}>
+          Reset
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearList}
+        >
+          clear items
+        </button>
+      )}
     </div>
   );
 };
